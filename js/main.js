8fec8d9c875d6492bc4d228c49ef1398eacb04d6
Marginall <mistmee2016@gmail.com>
2019-06-11 21:48:24 +0300

    fix const

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 'use strict';
 var items = [];
-var maxWidth = 1200;
-var minWidth = 1;
-var maxHeight = 704;
-var minHeght = 1;
+var MAX_WIDTH = 1200;
+var MIN_WIDTH = 1;
+var MAX_HEIGHT = 704;
+var MIN_HEIGHT = 1;
+var WIZARDS = 8;
 var mapPins = document.querySelectorAll('.map__pins');
 
 function pointsGenerator () {
-  for(var i = 0; i < 8; i++) {
+  for(var i = 1; i <= WIZARDS; i++) {
     var item = {
       author: {
         avatar: 'img/avatars/user0' + i + '.png'
@@ -16,8 +17,8 @@ function pointsGenerator () {
         type: ['palace', 'flat', 'house', 'bungalo']
       },
       location: {
-        x: Math.floor(Math.random() * (maxWidth - minWidth)) + minWidth,
-        y: Math.floor(Math.random() * (maxHeight - minHeght)) + minHeght
+        x: Math.floor(Math.random() * (MAX_WIDTH - MIN_WIDTH)) + MIN_WIDTH,
+        y: Math.floor(Math.random() * (MAX_HEIGHT - MIN_HEIGHT)) + MIN_HEIGHT
       }
     };
     items.push(item);
@@ -25,14 +26,14 @@ function pointsGenerator () {
 }
 
 function randomizer (arr) {
-  return Math.floor(Math.random() * (arr.length - 1)) + 1;
+  return Math.floor(Math.random() * arr.length) + 1;
 }
 
 function pinGenerator () {
   var templatePin = document.querySelector('#pin').content;
   var fragment = document.createDocumentFragment();
 
-  for(var i = 1; i < items.length; i++) {
+  for(var i = 0; i < items.length; i++) {
     var element = templatePin.cloneNode(true);
     var button = element.querySelector('.map__pin');
     var image = element.querySelector('img');
@@ -40,7 +41,7 @@ function pinGenerator () {
     button.setAttribute('style', 'left:' + items[i].location.x + 'px;' + 'top:' + items[i].location.y + 'px;');
     image.setAttribute('src', items[i].author.avatar);
     image.setAttribute('alt', items[i].offer.type[(randomizer(items[i].offer.type))]);
-	fragment.appendChild(element);
+    fragment.appendChild(element);
   };
 
   mapPins[0].appendChild(fragment);
