51134f32b19b99f3dce562bde3c96c291223bef2
Marginall <mistmee2016@gmail.com>
2019-06-29 10:21:44 +0300

    fix lint

--- js/filtr.js
+++ js/filtr.js
@@ -6,10 +6,6 @@
   window.sort = false;
 
   var filterSort = function (arr, value) {
-    var items = arr.map(function (items, index, arr) {
-      return items;
-    });
-
     var sortFilter = items.filter(function (item) {
       if (value === 'any') {
         return item;
