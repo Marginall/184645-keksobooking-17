37eb0c4ec2130739fcf22996813fabaedb20cdda
Marginall <mistmee2016@gmail.com>
2019-07-23 23:16:46 +0300

    fix

--- js/filtr.js
+++ js/filtr.js
@@ -2,26 +2,50 @@
 
 (function () {
   var filter = document.querySelector('.map__filters');
-  var housingType = filter.querySelector('#housing-type');
-  window.sort = false;
+  var type = filter.querySelector('#housing-type');
+  var price = filter.querySelector('#housing-price');
+  var rooms = filter.querySelector('#housing-rooms');
+  var guests = filter.querySelector('#housing-guests');
 
-  var filterSort = function (arr, value) {
-    var sortFilter = arr.filter(function (item) {
+  window.sort = false;
+  var filterSort = function (arr, value, param) {
+    var sortData = arr.filter(function (item) {
       if (value === 'any') {
         return item;
       } else {
-        return item.offer.type === value;
+        // console.log(item.offer.guests);
+        switch (param) {
+          case 'type':
+            return item.offer.type === value;
+          case 'price':
+            return console.log('2');
+          case 'rooms':
+            return item.offer.rooms === parseInt(value);
+          case 'guests':
+            console.log(value);
+              return item.offer.guests === parseInt(value);
+          default:
+            throw new Error('Неизвестный тип');
+        }
       }
     });
 
     window.sort = true;
-    window.pinGenerate(sortFilter);
-    window.offerGenerate(sortFilter);
+    window.pinGenerate(sortData);
   };
 
   filter.addEventListener('change', function (evt) {
-    if (evt.target === housingType) {
-      filterSort(window.items, evt.target.value);
+    switch (evt.target) {
+      case type:
+        return filterSort(window.items, evt.target.value, 'type');
+      case price:
+        return filterSort(window.items, evt.target.value, 'price');
+      case rooms:
+        return filterSort(window.items, evt.target.value, 'rooms');
+      case guests:
+        return filterSort(window.items, evt.target.value, 'guests');
+      default:
+        throw new Error('Неизвестный тип');
     }
   });
 })();
