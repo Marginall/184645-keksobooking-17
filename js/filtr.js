e5c6fda27e1570fa3dbb804ac202561367bdaf69
Marginall <mistmee2016@gmail.com>
2019-07-28 22:56:37 +0300

    fix

--- js/filtr.js
+++ js/filtr.js
@@ -6,42 +6,51 @@
   var price = filter.querySelector('#housing-price');
   var rooms = filter.querySelector('#housing-rooms');
   var guests = filter.querySelector('#housing-guests');
-  var sortData = null;
-  window.sort = false;
+  window.sort = {};
 
-  var filterSort = function (arr, value, param) {
-    sortData = arr.filter(function (item) {
-      if (value === 'any') {
-        return item;
-      } else {
-        switch (param) {
-          case 'type':
-            return item.offer.type === value;
-          case 'price':
-            return filterPrice(item, value);
-          case 'rooms':
-            return item.offer.rooms === parseInt(value);
-          case 'guests':
-            console.log(value);
-              return item.offer.guests === parseInt(value);
-          default:
-            throw new Error('Неизвестный тип');
-        }
+  var filterSort = function (arr) {
+    var sortItems = [];
+
+    window.sort = {
+      type: type.value === 'any' ? true : type.value,
+      price: price.value === 'any' ? window.sort.price = true : filterPrice(price , price.value),
+      rooms: rooms.value === 'any' ? true : parseInt(rooms.value),
+      guests: guests.value === 'any' ? true : parseInt(guests.value),
+      features: filterFeature()
+    };
+
+    arr.forEach(function (item) {
+      var params = {
+        type: item.offer.type === window.sort.type || window.sort.type === true,
+        rooms: item.offer.rooms === window.sort.rooms || window.sort.rooms === true,
+        guests: item.offer.guests === window.sort.guests || window.sort.guests === true,
+        features : true
+      };
+      filterFeatures(item, params);
+
+      if (params.type && params.rooms && params.guests && params.features) {
+        sortItems.push(item);
       }
     });
 
-    var pinUpdate = function () {
+    console.log(sortItems);
+    window.pinGenerate(sortItems);
+  };
 
-    };
-
-    window.sort ? pinUpdate(sortData) : window.pinGenerate(sortData);
-    window.sort = true;
+  var filterFeatures = function (item, params) {
+    window.sort.features.forEach(function (feature, i) {
+      if (item.offer.features.indexOf(feature)) {
+        params.features = false;
+      } else {
+        params.features = true;
+      }
+    });
   };
 
   var filterPrice = function (item, value) {
     switch (value) {
       case 'middle':
-        return item.offer.price > 10000 && item.offer.price < 50000;
+        item.value > 10000 && item.value < 50000;
       case 'low':
         return item.offer.price < 10000;
       case 'high':
@@ -51,18 +60,18 @@
     }
   };
 
-  filter.addEventListener('change', function (evt) {
-    switch (evt.target) {
-      case type:
-        return window.sort ? filterSort(sortData, evt.target.value, 'type') : filterSort(window.items, evt.target.value, 'type');
-      case price:
-        return window.sort ? filterSort(sortData, evt.target.value, 'price') : filterSort(window.items, evt.target.value, 'price');
-      case rooms:
-        return window.sort ? filterSort(sortData, evt.target.value, 'rooms') : filterSort(window.items, evt.target.value, 'rooms');
-      case guests:
-        return window.sort ? filterSort(sortData, evt.target.value, 'guests') : filterSort(window.items, evt.target.value, 'guests');
-      default:
-        throw new Error('Неизвестный тип');
+  var filterFeature = function () {
+    var features = filter.querySelectorAll('#housing-features input:checked');
+    var featuresList = [];
+    if (features.length) {
+      features.forEach(function (item) {
+        featuresList.push(item.value);
+      });
     }
+    return featuresList;
+  };
+
+  filter.addEventListener('change', function () {
+    filterSort(window.items);
   });
 })();
